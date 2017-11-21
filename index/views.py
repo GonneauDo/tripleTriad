58d6ae97e96710156161630c6115de1e137a4b44
Th3Merxx <th3merx@merx.ml>
2017-11-21 14:58:58 +0100

    ajout index

--- index/views.py
+++ index/views.py
@@ -1,6 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, render_to_response
+from django.template import RequestContext
 
 # Create your views here.
 
 def post_list(request):
 	return render(request, 'index.html')
+
+def index(request):
+	return render_to_response("index/index/html", context_instance=RequestContext(request))
