f46f5922135fdf59072caf48d71e219b62932adb
Doshirae <doshirae@lilo.org>
2017-12-02 22:15:56 +0100

    Début de jeu qui fonctionne

--- Triad.py
+++ Triad.py
@@ -1,35 +1,49 @@
 from random import randint
 
 class Carte:
-    def __init__(self, haut, droite, bas, gauche):
+    id = 1
+    def __init__(self, joueur, droite, haut, bas, gauche):
+        self.id = Carte.id
+        Carte.id += 1
+
+        self.joueur = joueur
         self.haut = haut
         self.droite = droite
         self.bas = bas
         self.gauche = gauche
 
     def __str__(self):
-        return "← {gauche} ↑ {haut} ↓ {bas} → {droite}".format(
+        return "({joueur})← {gauche} ↑ {haut} ↓ {bas} → {droite}".format(
             haut = self.haut,
             droite = self.droite,
             bas = self.bas,
-            gauche = self.gauche
+            gauche = self.gauche,
+            joueur = self.joueur
         )
 
-    def bataille(self, other):
+    def bataille(self, other, position):
         """
         retourne le gagnant d'une bataille
         A invoquer quand une carte est posée
+        position, c'est la position de other
         """
-        if self.haut > other.bas or self.droite > other.gauche or self.gauche > other.droite or self.bas > other.haut:
-            return self
+        if other == None: # other est soit pas posée, soit en dehors du terrain
+            return self.id
         else:
-            return other
+            if position == "haut":
+                return self.haut > other.bas
+            elif position == "bas":
+                return self.bas > other.haut
+            elif position == "gauche":
+                return self.gauche > other.droite
+            elif position == "droite":
+                return self.droite > other.gauche
 
 class Grille:
     def __init__(self):
         cartes = []
         for i in range(9):
-            cartes.append("."*16)
+            cartes.append(None)
             self.cartes = cartes
 
     def __iter__(self):
@@ -38,15 +52,65 @@ class Grille:
 
     def __str__(self):
         return "\n\n".join([
-            "{c[0]}\t{c[1]}\t{c[2]}",
-            "{c[3]}\t{c[4]}\t{c[5]}",
-            "{c[6]}\t{c[7]}\t{c[8]}\n\n\n"
+            "{c[0]} \t{c[1]} \t{c[2]}",
+            "{c[3]} \t{c[4]} \t{c[5]}",
+            "{c[6]} \t{c[7]} \t{c[8]}\n\n\n"
         ]).format(
             c = self.cartes
         )
 
+    def __getitem__(self, index):
+        return self.cartes[index]
+
+    def __setitem__(self, index, valeur):
+        self.cartes[index] = valeur
+
+    def poser(self, carte, index):
+        """
+        0 1 2
+        3 4 5
+        6 7 8
+        """
+        """
+        en haut -> -3
+        en bas -> +3
+        a gauche -> -1
+        a droite -> +1
+        """
+
+        voisins = {}
+        voisins["haut"] = self[index-3] if index-3 >= 0 else None
+        voisins["bas"] = self[index+3] if index+3 < 9 else None
+        voisins["gauche"] = self[index+1] if index+1 < 9 else None
+        voisins["droite"] = self[index-1] if index-1 >= 0 else None
+
+        # La c'est un tableau donc la référence marche
+        if carte.bataille(voisins["haut"], "haut") and voisins["haut"]:
+            voisins["haut"].joueur = carte.joueur
+        if carte.bataille(voisins["bas"], "bas") and voisins["bas"]:
+            voisins["bas"].joueur = carte.joueur
+        if carte.bataille(voisins["gauche"], "gauche") and voisins["gauche"]:
+            voisins["gauche"].joueur = carte.joueur
+        if carte.bataille(voisins["droite"], "droite") and voisins["droite"]:
+            voisins["droite"].joueur = carte.joueur
+
+        self[index] = carte
+
+
+
 if __name__ == "__main__":
     grille = Grille()
+
     print(grille)
-    grille.cartes[3] = Carte(1, 2, 10, 4)
+    c1 = Carte(1, 10, 2, 3, 4)
+    grille.poser(c1, 3)
     print(grille)
+
+    c2 = Carte(2, 3, 2, 1, 4)
+    grille.poser(c2, 5)
+    print(grille)
+
+    c2 = Carte(2, 3, 4, 1, 4)
+    grille.poser(c2, 6)
+    print(grille)
+
