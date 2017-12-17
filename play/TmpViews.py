9c82fb38fa14742bd181a0468a5939a1e2b23127
Do <pi@merx.ml>
2017-12-17 04:11:39 +0100

    affichage du jeu (texte)

--- play/TmpViews.py
+++
@@ -1,7 +0,0 @@
-from django.http import HttpResponse
-from random import randrange as rand
-
-def post_list(request):
-	score=rand(6)
-	html="<html><body><p style=color:red>Score : %s</p></body></html>" %score
-	return HttpResponse(html)
